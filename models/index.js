e590bd5665125463dc267ffbf5c464f20054da57
Xav <xavierjmashton@gmail.com>
2024-11-18 08:33:12 +0100

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const sequelize = require('../core/orm');
+const type = require('./type');
+const task = require('./task');
+
+task.hasOne(type);
+type.hasMany(task);
+
+//sequelize.sync({ alter: true });
+
+module.exports = {
+    type,
+    task,
+};
