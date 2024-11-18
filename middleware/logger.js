193143ba84da6b08ad9211cfb83e9cd3aa4d3cf1
Xavier Ashton <xavierjmashton@gmail.com>
2024-11-18 11:52:51 +0100

    updated the file to have the timestamp in filename

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const logFilePath = path.join(__dirname, 'logs.txt');
+const getLogFilePath = () => {
+    const date = new Date().toISOString().split('T')[0]; // Get the date part of the ISO string
+    return path.join(__dirname, `logs-${date}.txt`);
+};
 
 const logger = (req, res, next) => {
     const logMessage = `[${new Date().toISOString()}] Method: ${req.method}, URL: ${req.url}\n`;
+    const logFilePath = getLogFilePath();
     fs.appendFile(logFilePath, logMessage, (err) => {
         if (err) {
             console.error('Failed to write to log file:', err);
