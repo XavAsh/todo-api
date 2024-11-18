351aaa90507b8fc35f0570ddabf2509860fb3788
Xav <xavierjmashton@gmail.com>
2024-11-18 10:58:48 +0100

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    console.log('Connected to the API');
-    res.json({ message: 'Connected to the API' });
-});
-
-module.exports = router;
